d67bce0aa1e394e0a56d4adc81ccdae5cb96e935
csw823197541 <823917541@qq.com>
2017-11-15 14:10:21 +0800

    1

--- src/com/shbtos/biz/smart/cwp/service/SmartCwpValidatorResults.java
+++ src/com/shbtos/biz/smart/cwp/service/SmartCwpValidatorResults.java
@@ -1,7 +1,9 @@
 package com.shbtos.biz.smart.cwp.service;
 
+import com.shbtos.biz.smart.cwp.pojo.Results.SmartReCwpBlockInfo;
 import com.shbtos.biz.smart.cwp.pojo.Results.SmartReCwpValidatorInfo;
 import com.shbtos.biz.smart.cwp.pojo.Results.SmartReMessageInfo;
+import com.shbtos.biz.smart.cwp.pojo.SmartCwpWorkBlockInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +17,12 @@ public class SmartCwpValidatorResults {
     private SmartReMessageInfo smartReMessageInfo;
 
     private List<SmartReCwpValidatorInfo> smartReCwpValidatorInfoList;
+    private List<SmartCwpWorkBlockInfo> smartCwpWorkBlockInfoList;
 
     public SmartCwpValidatorResults() {
         smartReMessageInfo = new SmartReMessageInfo();
         smartReCwpValidatorInfoList = new ArrayList<>();
+        smartCwpWorkBlockInfoList = new ArrayList<>();
     }
 
     public SmartReMessageInfo getSmartReMessageInfo() {
@@ -35,5 +39,13 @@ public class SmartCwpValidatorResults {
 
     public void setSmartReCwpValidatorInfoList(List<SmartReCwpValidatorInfo> smartReCwpValidatorInfoList) {
         this.smartReCwpValidatorInfoList = smartReCwpValidatorInfoList;
+    }
+
+    public List<SmartCwpWorkBlockInfo> getSmartCwpWorkBlockInfoList() {
+        return smartCwpWorkBlockInfoList;
+    }
+
+    public void setSmartCwpWorkBlockInfoList(List<SmartCwpWorkBlockInfo> smartCwpWorkBlockInfoList) {
+        this.smartCwpWorkBlockInfoList = smartCwpWorkBlockInfoList;
     }
 }
