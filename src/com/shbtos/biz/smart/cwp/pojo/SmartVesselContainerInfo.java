596eb712cf8cc3181ec5be509a852d4137a3c386
csw823197541 <823917541@qq.com>
2018-04-13 10:47:16 +0800

    进出口船图箱对象新增字段

--- src/com/shbtos/biz/smart/cwp/pojo/SmartVesselContainerInfo.java
+++ src/com/shbtos/biz/smart/cwp/pojo/SmartVesselContainerInfo.java
@@ -56,6 +56,7 @@ public class SmartVesselContainerInfo {
     private String dispatchedTask; //该指令是否已经派遣任务，Y表示已派遣；N表示没有，如已经产生AGV、ASC调度等任务
     private String canRecycleFlag; //指令是否可以回收标记，Y或者为空表示可以回收；N表示不可以回收，应该继续执行下去
     private String recycleWiFlag; //回收重排的指令标记，Y表示回收的指令；N或者null表示非回收的指令
+    private Long cwpBlockId; //指令所在作业块的Id号
     private String directCntFlag; //直装箱标记，Y表示箱子标记为直装箱，N或者null表示非直装箱子
     private String dispatchedWiInfo; //表示该箱子是否派发了指令任务（如转堆），有的话发箱的时候暂时不发送，Y表示有wi，N或者空表示没有wi任务
 
@@ -457,5 +458,13 @@ public class SmartVesselContainerInfo {
 
     public void setDispatchedWiInfo(String dispatchedWiInfo) {
         this.dispatchedWiInfo = dispatchedWiInfo;
+    }
+
+    public Long getCwpBlockId() {
+        return cwpBlockId;
+    }
+
+    public void setCwpBlockId(Long cwpBlockId) {
+        this.cwpBlockId = cwpBlockId;
     }
 }
